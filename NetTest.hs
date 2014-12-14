c85ad50d929dd188ed5d3b46d81b47aaf52c7641
dbanerjee1979 <dbanerje1979+github@gmail.com>
2014-12-13 20:49:30 -0600

    Rename code to command

--- NetTest.hs
+++ NetTest.hs
@@ -6,11 +6,10 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Map.Strict as Map
 
-data Message = Message { sender :: Maybe String
-                       , code   :: String
-                       , params :: [String]
+data Message = Message { sender   :: Maybe String
+                       , command  :: String
+                       , params   :: [String]
                        } deriving (Show)
-
 
 main :: IO ()
 main = do
@@ -36,7 +35,7 @@ main = do
     let s4 = "NOTICE\r\n"
     putStrLn $ show $ parse s4
 
-data ParseState = Start | Name | Code | Param | ParamM | ParamT
+data ParseState = Start | Name | Command | Param | ParamM | ParamT
 data ValueType = One String | Many [String]
 
 parse :: String -> Maybe Message
@@ -44,30 +43,30 @@ parse [] = Nothing
 parse m  = build Start m "" $ Map.fromList [("params", (Many []))]
 
 build :: ParseState -> String -> String -> Map.Map String ValueType -> Maybe Message
-build Start  (':':cs)  value values        = build Name cs "" values
-build Start  (c:cs)    value values        = build Code cs [c] values
-build Name   (' ':cs)  value values        = build Code cs "" $ appendSender value values
-build Name   (c:cs)    value values        = build Name cs (value ++ [c]) values
-build Code   (' ':cs)  value values        = build Param cs "" $ appendCode value values
-build Code   ('\r':cs) value values        = makeMessage $ appendCode value values
-build Code   ('\n':cs) value values        = makeMessage $ appendCode value values
-build Code   (c:cs)    value values        = build Code cs (value ++ [c]) values
-build Param  (':':cs)  value values        = build ParamT cs "" values 
-build Param  (c:cs)    value values        = build ParamM cs [c] values
-build ParamT ('\r':cs) value values        = makeMessageParams value values
-build ParamT ('\n':cs) value values        = makeMessageParams value values
-build ParamT (c:cs)    value values        = build ParamT cs (value ++ [c]) values
-build ParamM (' ':cs)  value values        = build Param cs "" (appendParams value values)
-build ParamM ('\r':cs) value values        = makeMessageParams value values
-build ParamM ('\n':cs) value values        = makeMessageParams value values
-build ParamM (c:cs)    value values        = build ParamM cs (value ++ [c]) values
-build _      _         _     _             = Nothing
+build Start   (':':cs)  value values        = build Name cs "" values
+build Start   (c:cs)    value values        = build Command cs [c] values
+build Name    (' ':cs)  value values        = build Command cs "" $ appendSender value values
+build Name    (c:cs)    value values        = build Name cs (value ++ [c]) values
+build Command (' ':cs)  value values        = build Param cs "" $ appendCommand value values
+build Command ('\r':cs) value values        = makeMessage $ appendCommand value values
+build Command ('\n':cs) value values        = makeMessage $ appendCommand value values
+build Command (c:cs)    value values        = build Command cs (value ++ [c]) values
+build Param   (':':cs)  value values        = build ParamT cs "" values 
+build Param   (c:cs)    value values        = build ParamM cs [c] values
+build ParamT  ('\r':cs) value values        = makeMessageParams value values
+build ParamT  ('\n':cs) value values        = makeMessageParams value values
+build ParamT  (c:cs)    value values        = build ParamT cs (value ++ [c]) values
+build ParamM  (' ':cs)  value values        = build Param cs "" (appendParams value values)
+build ParamM  ('\r':cs) value values        = makeMessageParams value values
+build ParamM  ('\n':cs) value values        = makeMessageParams value values
+build ParamM  (c:cs)    value values        = build ParamM cs (value ++ [c]) values
+build _       _         _     _             = Nothing
 
 appendSender:: String -> Map.Map String ValueType -> Map.Map String ValueType
 appendSender value values = Map.insert "sender" (One value) values
 
-appendCode :: String -> Map.Map String ValueType -> Map.Map String ValueType
-appendCode value values = Map.insert "code" (One value) values
+appendCommand :: String -> Map.Map String ValueType -> Map.Map String ValueType
+appendCommand value values = Map.insert "command" (One value) values
 
 appendParams :: String -> Map.Map String ValueType -> Map.Map String ValueType
 appendParams value values = Map.insert "params" (Many v) values
@@ -82,12 +81,12 @@ makeMessageParams value values =
 
 makeMessage ::  Map.Map String ValueType -> Maybe Message
 makeMessage values = do 
-    code <- Map.lookup "code" values
+    command <- Map.lookup "command" values
     params <- Map.lookup "params" values
-    return Message { sender = do v <- Map.lookup "sender" values
-                                 return $ extract v
-                   , code = extract $ code
-                   , params = extractAll $ params
+    return Message { sender   = do v <- Map.lookup "sender" values
+                                   return $ extract v
+                   , command = extract $ command
+                   , params  = extractAll $ params
                    }
 
 extract :: ValueType -> String
@@ -95,16 +94,6 @@ extract (One a) = a
 
 extractAll :: ValueType -> [String]
 extractAll (Many a) = a
-
-{-
-build Name  ' ' cs sender code param params        = build Start (C.head cs) (B.tail cs) sender code param params
-build Name  c   cs (Just sender) code param params = build Name (C.head cs) (B.tail cs) (Just $ sender ++ [c]) code param params
-build Code  ' ' cs sender code                     = 
-build Code  ' ' cs sender code              = Just Message { sender = sender
-                                                           , code = code
-                                                           }
-build Code  c   cs sender code              = build Code (C.head cs) (B.tail cs) sender (code ++ [c])
--}
 
 address :: String -> Int -> IO AddrInfo
 address hostname port = do
