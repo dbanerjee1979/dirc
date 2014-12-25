d0a9ae573e989f82d91fcd06e5fce8e13b637938
dbanerjee1979 <dbanerje1979+github@gmail.com>
2014-12-24 22:45:44 -0600

    Populate buffer from message send

--- Dirc.hs
+++ Dirc.hs
@@ -8,6 +8,14 @@ import Graphics.UI.Gtk.Builder
 import Reactive.Banana
 import Reactive.Banana.Gtk
 
+type EventSource a = (AddHandler a, a -> IO ())
+
+addHandler :: EventSource a -> AddHandler a
+addHandler = fst
+
+fire :: EventSource a -> a -> IO ()
+fire = snd
+
 main :: IO ()
 main = do
     initGUI
@@ -17,17 +25,27 @@ main = do
     closeBtn <- builderGetObject bld castToButton "close-button"
     msgTxt <- builderGetObject bld castToTextView "message-text"
     buffer <- textViewGetBuffer msgTxt
-    textBufferInsertAtCursor buffer "Hi\n"
+
+    esmsg <- newAddHandler
 
     let networkDescription :: forall t. Frameworks t => Moment t ()
         networkDescription = do
             eclose <- eventM closeBtn Gtk.buttonReleaseEvent
             edelete <- eventM dlg Gtk.deleteEvent
+            emsg <- fromAddHandler (addHandler esmsg)
 
+            reactimate $ (handleMsg buffer) <$> emsg
             reactimate $ Gtk.mainQuit <$ eclose
             reactimate $ Gtk.mainQuit <$ edelete
     network <- compile networkDescription
     actuate network
 
+    fire esmsg "Hi!"
+
     widgetShowAll dlg
     mainGUI
+
+handleMsg :: TextBufferClass self => self -> String -> IO ()
+handleMsg buffer msg = do
+    textBufferInsertAtCursor buffer msg
+    textBufferInsertAtCursor buffer "\n"
